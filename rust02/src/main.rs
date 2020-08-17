85610621b0da5ddfa68ee86b0a3abe48932ec0c8
martincrb <martincristobaltdr@gmail.com>
2020-08-17 20:25:13 +0200

    feat: experimented with some var types

--- rust02/src/main.rs
+++ rust02/src/main.rs
@@ -1,4 +1,8 @@
 mod print;
+mod vars;
+mod types;
 fn main() {
     print::run();
+    vars::run();
+    types::run();
 }
--- rust02/src/vars.rs
+++ rust02/src/vars.rs
@@ -1,3 +1,9 @@
 pub fn run() {
-
+    // Variables are immutable by default in Rust
+    let name = "Martin";
+    println!("My name is {}", name);
+    
+    const ID: i32 = 001;
+    // I can assign multiple vars using this kind of 'destructuring'
+    let (var1, var2) = ("string", 3);
 }
